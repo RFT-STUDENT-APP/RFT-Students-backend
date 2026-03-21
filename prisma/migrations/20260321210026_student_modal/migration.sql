43ddbf537491aeb2ecb96469b1016d0d7cef8854
obuastella <stellaobua123@gmail.com>
2026-03-21 22:03:15 +0100

    setup model for students and setup be

---
+++ prisma/migrations/20260321210026_student_modal/migration.sql
@@ -0,0 +1,23 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('STUDENT', 'COURSE_REP', 'LECTURER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "Student" (
+    "id" SERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "matricNumber" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Student_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Student_email_key" ON "Student"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Student_matricNumber_key" ON "Student"("matricNumber");
